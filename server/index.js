52be0ae1d450f33acf468b97ec7b9e3c990e862a
GabrielDeMiranda <gm851@live.mdx.ac.uk>
2021-02-14 11:33:15 +0000

    cleaning up

--- server/index.js
+++ server/index.js
@@ -28,4 +28,4 @@ if (process.env.NODE_ENV !== 'development') {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Server is online on port 3000'));
+app.listen(port, () => console.log('Listening on port ${port}...'));
