f55fd8677383c91439db767ab5c93e237d39d5fb
GabrielDeMiranda <gm851@live.mdx.ac.uk>
2021-02-14 10:26:48 +0000

    waking up

--- server/data/db.js
+++ server/data/db.js
@@ -1,10 +1,7 @@
 const mongodb = require('mongodb');
 
 const connect = async (nameCollecion) => {
-    const clusterName = process.env.CLUSTER;
-    const password = process.env.PASSWORD;
-    const database = process.env.DATABASE;
-    const root = process.env.ROOT;
+   
 
     const client = await mongodb.connect(
         `mongodb+srv://MyMongoDBUser:mongodb12345@gabriel-cw2.dyfen.mongodb.net/webstore?retryWrites=true&w=majority`,
