94d20f851d7cc433aa56fb74ea62a39442c34af4
GabrielDeMiranda <gm851@live.mdx.ac.uk>
2021-02-14 10:28:13 +0000

    waking up - deploy heroku - test

--- server/data/db.js
+++ server/data/db.js
@@ -1,13 +1,16 @@
 const mongodb = require('mongodb');
 
 const connect = async (nameCollecion) => {
-   
+    const clusterName = process.env.CLUSTER;
+    const password = process.env.PASSWORD;
+    const database = process.env.DATABASE;
+    const root = process.env.ROOT;
 
     const client = await mongodb.connect(
         `mongodb+srv://MyMongoDBUser:mongodb12345@gabriel-cw2.dyfen.mongodb.net/webstore?retryWrites=true&w=majority`,
         { useUnifiedTopology: true }
     );
-
+//bug
     return client.db(database).collection(nameCollecion);
 }
 
